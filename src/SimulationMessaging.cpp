a9043c7e94f6727beed5205c1c1943006b4e9322
Chris Bontempi <bontempi@uchc.edu>
2026-06-14 12:48:29 -0400

    Allowed the use of curl library on Windows.

--- src/SimulationMessaging.cpp
+++ src/SimulationMessaging.cpp
@@ -9,8 +9,8 @@ using std::endl;
 
 #include <cstdio>
 #ifdef USE_MESSAGING
-#if ( !defined(WIN32) && !defined(WIN64) ) // UNIX
 #include <curl/curl.h>
+#if ( !defined(WIN32) && !defined(WIN64) ) // UNIX
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
